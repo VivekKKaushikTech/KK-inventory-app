dedc12126ae4d6b10139a33093d52215e15a777e
Ravi Piplani <ravi.piplani@progcap.com>
2025-02-05 23:34:55 +0530

    code review

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Input = ({ type, placeholder, className }) => {
+const Input = ({ type, placeholder, value, maxLength, onChange, className }) => {
   return (
     <input
       type={type}
+      value={value}
+      maxLength={maxLength}
+      onChange={onChange}
       placeholder={placeholder}
       className={`border p-2 rounded w-full ${className}`}
     />
